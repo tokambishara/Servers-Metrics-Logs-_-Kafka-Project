2f0018250d080ed6924bc3ff2e2df830eaa7508b
tokambishara <91013560+tokambishara@users.noreply.github.com>
2025-05-10 01:21:42 +0300

    Update log_consumer_HDFS.py

--- log_consumer_HDFS.py
+++ log_consumer_HDFS.py
@@ -6,20 +6,18 @@ import time
 # HDFS client setup
 client = InsecureClient('http://localhost:9870/', user='root')
 
-# اسم الفايل اللي هنقرأ منه
 local_file = 'output_log_summary.csv'
-hdfs_path = 'HDFS_Proj/output_log_summary.csv'  # نفس الاسم على HDFS
+hdfs_path = 'HDFS_Proj/output_log_summary.csv' 
 
 while True:
     try:
         df = pd.read_csv(local_file)
 
-        # overwrite نفس الملف
         with client.write(hdfs_path, encoding='utf-8', overwrite=True) as writer:
             df.to_csv(writer, index=False)
 
-        print(f"✔️ Updated {hdfs_path} on HDFS")
+        print(f"Updated {hdfs_path} on HDFS")
     except Exception as e:
-        print(f"❌ Error: {e}")
+        print(f"Error: {e}")
 
     time.sleep(300) 
